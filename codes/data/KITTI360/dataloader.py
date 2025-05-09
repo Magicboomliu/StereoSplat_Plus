a877cd183eeff4cf1bb5af006ef9ae1588ccda86
MagicBoomLiu4GPUs <zliu@ok.sc.e.titech.ac.jp>
2025-05-09 22:02:56 +0900

    add

--- codes/data/KITTI360/dataloader.py
+++ codes/data/KITTI360/dataloader.py
@@ -31,15 +31,51 @@ from model.utils.ops import get_cam_info_gaussian, get_ray_directions, get_rays
 
 
 class KITTI360(Dataset):
+    
     def __init__(
         self,
+        datapath:str,
+        data_version:str,
         resolution: list = [224, 400],
         split: str = "train",
+        sequence:str ='2013_05_28_drive_0000_sync',
         use_center: bool = True,
         use_first: bool = False,
-        use_last: bool = False):
+        use_last: bool = False,
+        **kwargs,
+        ):
         super().__init__()
+
+
+        self.camera_types = [
+            "CAM_LEFT",
+            "CAM_RIGHT",
+            "LIDAR_TOP"
+        ]
+
+        self.reso = resolution # 是否使用每个 bin 中的中间帧/最前帧/最后帧
         
+        # using the center as the input
+        self.use_center = use_center
+        
+        # using the last or first for input， or just for validations
+        self.use_first = use_first
+        self.use_last = use_last
+        
+        if split =='train':
+            pass
+        
+        elif split =="val":
+            pass
+        
+        elif split =='test':
+            pass
+        
+        elif split =='demo':
+            pass
+            
+        self.split = split
+
 
 
     def __getitem__(self, index):
@@ -52,4 +88,9 @@ class KITTI360(Dataset):
 
 
 if __name__=="__main__":
-    pass
+    
+    dataset_params = {
+        
+        
+    }
+    
