280f4abe1b4453e22d6602f07a49317d1dbbba77
MagicBoomLiu4GPUs <zliu@ok.sc.e.titech.ac.jp>
2025-05-12 18:27:59 +0900

    add

--- codes/data/KITTI360/dataloader.py
+++ codes/data/KITTI360/dataloader.py
@@ -28,15 +28,25 @@ from model.utils.image import resize_image, HWC3
 from model.utils.typing import *
 from model.utils.camera import get_camera, rescale_intrisic
 from model.utils.ops import get_cam_info_gaussian, get_ray_directions, get_rays
+from data.KITTI360.transforms.loading import load_info, load_conditions
 
 
-class KITTI360(Dataset):
+def read_text_lines(filepath):
+    with open(filepath, 'r') as f:
+        lines = f.readlines()
+    lines = [l.rstrip() for l in lines]
+    return lines
+
+class KITTI360Dataset(Dataset):
     
     def __init__(
         self,
         datapath:str,
+        train_filelist:str,
+        val_filelist:str,
+        test_filelist:str,
         data_version:str,
-        resolution: list = [224, 400],
+        resolution: list,
         split: str = "train",
         sequence:str ='2013_05_28_drive_0000_sync',
         use_center: bool = True,
@@ -46,15 +56,26 @@ class KITTI360(Dataset):
         ):
         super().__init__()
 
-
+        self.datapath = datapath
+        self.data_version = data_version
+        
         self.camera_types = [
             "CAM_LEFT",
-            "CAM_RIGHT",
-            "LIDAR_TOP"
+            "CAM_RIGHT"
         ]
 
-        self.reso = resolution # 是否使用每个 bin 中的中间帧/最前帧/最后帧
+        self.camera_types_first = [
+            "CAM_LEFT",
+            "CAM_RIGHT"
+        ]
+        self.camera_types_last = [
+            "CAM_LEFT",
+            "CAM_RIGHT"
+        ]
         
+        self.reso = resolution
+        self.resize_reso = resolution[0]
+        self.original_reso = resolution[1]
         # using the center as the input
         self.use_center = use_center
         
@@ -63,40 +84,113 @@ class KITTI360(Dataset):
         self.use_last = use_last
         
         if split =='train':
-            pass
+            self.bin_tokens = read_text_lines(train_filelist)
         
         elif split =="val":
-            pass
+            self.bin_tokens = read_text_lines(val_filelist)
         
         elif split =='test':
-            pass
+            self.bin_tokens = read_text_lines(val_filelist)
         
         elif split =='demo':
-            pass
+            raise NotImplementedError
             
         self.split = split
 
-
-
     def __getitem__(self, index):
+        
+        bin_token_name = self.bin_tokens[index]
+        abs_bin_token_fname = os.path.join(self.datapath,"feedforward_bins",self.data_version,bin_token_name)
+        
+        bin_info = self._load_pkl_file(abs_bin_token_fname)
+        # print(bin_info['token']) # scene2013_05_28_drive_0000_sync_bin000 
+        # print(bin_info['scene_token']) # 2013_05_28_drive_0000_sync
+        # print(bin_info['timestep']) # 0000000256.
+        # print(bin_info['bin_length']) # 8.402467621701142
+        # print(bin_info['sensor_info'].keys()) # dict_keys(['LIDAR_TOP', 'CAM_LEFT', 'CAM_RIGHT'])
+        
+        # center
+        sensor_info_center = {sensor: bin_info["sensor_info"][sensor][0] for sensor in self.camera_types + ["LIDAR_TOP"]}
+        # first 
+        sensor_info_first = {sensor: bin_info["sensor_info"][sensor][1] for sensor in self.camera_types_first + ["LIDAR_TOP"]}
+        # last
+        sensor_info_last = {sensor: bin_info["sensor_info"][sensor][2] for sensor in self.camera_types_last + ["LIDAR_TOP"]}
 
-        pass
+        # =================== Input views of this bin ===================== #
+        input_img_paths, input_c2ws, input_w2cs = [], [], []
+        if self.use_center:
+            for cam in self.camera_types:
+
+                info = copy.deepcopy(sensor_info_center[cam]) # all the infors
+                img_path, c2w, w2c = load_info(info)
+                img_path = os.path.join(self.datapath,img_path)
+                assert os.path.exists(img_path)
+                input_img_paths.append(img_path)
+                input_c2ws.append(c2w)
+                input_w2cs.append(w2c)
+
+        if self.use_first:
+            for cam in self.camera_types_first:
+                info = copy.deepcopy(sensor_info_first[cam])
+                img_path, c2w, w2c = load_info(info)
+                img_path = os.path.join(self.datapath,img_path)
+                input_img_paths.append(img_path)
+                input_c2ws.append(c2w)
+                input_w2cs.append(w2c)
+                
+        if self.use_last:
+            for cam in self.camera_types_last:
+                info = copy.deepcopy(sensor_info_last[cam])
+                img_path, c2w, w2c = load_info(info)
+                img_path = os.path.join(self.datapath,img_path)
+                input_img_paths.append(img_path)
+                input_c2ws.append(c2w)
+                input_w2cs.append(w2c)
+
+        input_c2ws = torch.as_tensor(input_c2ws, dtype=torch.float32) #(2,4,4)----> Center Frame
+        input_w2cs = torch.as_tensor(input_w2cs, dtype=torch.float32) #(2,4,4)----> Center Frame
+
+
+        input_imgs, input_depths, input_depths_m, input_confs_m, input_cks = \
+                    load_conditions(input_img_paths, self.reso)      
+
+        input_cks = torch.as_tensor(input_cks, dtype=torch.float32) #(6,3,3)--> Camera Intrinsics
+        
+        
+
+        
+
+        
+        
+
+    def _load_pkl_file(self,path):
+        with open(path, 'rb') as f:
+            data_dict = pkl.load(f)
+        return data_dict
+
 
     def __len__(self):
-        return 
+        return len(self.bin_tokens)
 
 
 
 if __name__=="__main__":
     
     dataset_params = {
-        datapath:"/data1/StereoDatasets/KITTI/KITTI360/",
-        data_version:str,
-        resolution: list = [224, 400],
-        split: str = "train",
-        sequence:str ='2013_05_28_drive_0000_sync',
-        use_center: bool = True,
-        use_first: bool = False,
-        use_last: bool = False,
+        "datapath":"/data1/StereoDatasets/KITTI/KITTI360/",
+        "train_filelist":"/home/zliu/Project2025/FeedStereoGS/filenames/kitti360/trainval/train_2013_05_28_drive_0000_sync.txt",
+        "val_filelist":"/home/zliu/Project2025/FeedStereoGS/filenames/kitti360/trainval/val_2013_05_28_drive_0000_sync.txt",
+        "test_filelist":"/home/zliu/Project2025/FeedStereoGS/filenames/kitti360/trainval/val_2013_05_28_drive_0000_sync.txt",
+        "data_version":"bin_infos_8.0",
+        "resolution":[[224, 840],[376,1408]], # idx 0 is the proceseed image resolution, the last is the the initial image resolution
+        "split":"train",
+        "sequence":'2013_05_28_drive_0000_sync',
+        "use_center":True,
+        "use_first": False,
+        "use_last": False,
     }
     
+    dataset = KITTI360Dataset(**dataset_params)
+    
+    for idx, data in enumerate(dataset):
+        print(data)
---
+++ codes/data/KITTI360/transforms/loading.py
@@ -0,0 +1,155 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+import PIL
+from PIL import Image
+from model.utils.image import resize_image, HWC3
+from mmdet3d.structures.points import LiDARPoints, BasePoints, get_points_type
+from typing import List, Optional, Union
+import cv2
+import os.path as osp
+import json
+import copy
+import matplotlib.pyplot as plt
+import os
+
+
+
+def load_info(info):
+    img_path = info["data_path"]
+    # use lidar coordinate of the key frame as the world coordinate
+    c2w = info["sensor2lidar_transform"]
+    
+    
+    # opencv cam -> opengl cam, maybe not necessary!
+    flip_yz = np.eye(4)
+    flip_yz[1, 1] = -1
+    flip_yz[2, 2] = -1
+    c2w = c2w@flip_yz  # current c2w is the opengl coordinate
+
+    # lidar2cam rotatopnns
+    lidar2cam_r = np.linalg.inv(info["sensor2lidar_rotation"])
+    lidar2cam_t = info["sensor2lidar_translation"] @ lidar2cam_r.T
+    w2c = np.eye(4)
+    w2c[:3, :3] = lidar2cam_r.T
+    w2c[3, :3] = -lidar2cam_t
+    
+    return img_path, c2w, w2c
+
+
+
+def load_the_depthanytingV2_results(path,scale=50):
+    # Read the image in unchanged mode (preserves uint16 format)
+    img = np.array(cv2.imread(path, cv2.IMREAD_UNCHANGED)).astype(np.float32)
+    img = img/scale
+    return img
+
+
+
+def load_conditions(img_paths, reso):
+    
+    target_reso = reso[0]
+    original_reso = reso[1]
+    
+
+    def maybe_resize(img, tgt_reso, ck):
+        if not isinstance(img, PIL.Image.Image):
+            img = Image.fromarray(img)
+        resize_flag = False
+        if img.height != tgt_reso[0] or img.width != tgt_reso[1]:
+            # img.resize((w, h))
+            fx, fy, cx, cy = ck[0, 0], ck[1, 1], ck[0, 2], ck[1, 2]
+            scale_h, scale_w = tgt_reso[0] / img.height, tgt_reso[1] / img.width
+            fx_scaled, fy_scaled, cx_scaled, cy_scaled = fx * scale_w, fy * scale_h, cx * scale_w, cy * scale_h
+            ck = np.array([[fx_scaled, 0, cx_scaled], [0, fy_scaled, cy_scaled], [0, 0, 1]])
+            img = img.resize((tgt_reso[1], tgt_reso[0]))
+            resize_flag = True
+        return np.array(img), ck, resize_flag
+    
+    imgs, cks = [], []
+    depths = [] # depths normalized
+    depths_m = [] # depths with metric
+    confs_m = []  # confidence
+    
+    
+    for img_path in img_paths:      
+        if "image_00/data_rect" in img_path:
+            # left image
+            raw_ck = np.array([[552.554261,   0,       682.049453],
+                            [  0, 552.554261, 238.769549],
+                            [  0, 0,    1]]) # 3x3            
+        elif "image_01/data_rect" in img_path:
+            # right image
+            raw_ck = np.array([[552.554261,   0,       682.049453],
+                            [  0, 552.554261, 238.769549],
+                            [  0, 0,    1]]) # 3x3
+        
+        else:
+            raise NotImplementedError
+            
+        img = Image.open(img_path)
+        h, w = img.height, img.width
+        img, ck, resize_flag = maybe_resize(img, target_reso, raw_ck)
+        
+        img = HWC3(img)
+        imgs.append(img)
+        cks.append(ck)
+        
+        
+        # relative depth from DepthAnything-v2
+        # /data1/StereoDatasets/KITTI/KITTI360/data_2d_raw/2013_05_28_drive_0000_sync/image_00/data_rect/0000000256.png
+        # /data1/StereoDatasets/KITTI/KITTI360/monocular_depth/monodepthV2/data_2d_raw/2013_05_28_drive_0000_sync/
+        depth_path = img_path.replace("data_2d_raw", "monocular_depth/monodepthV2/data_2d_raw")
+        assert os.path.exists(depth_path)
+        
+        disp = load_the_depthanytingV2_results(depth_path)
+        if resize_flag:
+            disp = Image.fromarray(disp)
+            disp = disp.resize((reso[1], reso[0]), Image.BILINEAR)
+            disp = np.array(disp)
+        
+        # inverse disparity to relative depth
+        # clamping the farthest depth to 50x of the nearest
+        range = np.minimum(disp.max() / (disp.min() + 0.001), 50.0)
+        max = disp.max()
+        min = max / range
+        depth = 1 / np.maximum(disp, min)
+        depth = (depth - depth.min()) / (depth.max() - depth.min()) # range from 0 ~1
+        depths.append(depth)
+        
+        
+        # metric depth from Metric3D-v2
+        depthm_path = img_path.replace("sweeps_small", "sweeps_dptm_small")
+        depthm_path = depthm_path.replace("samples_small", "samples_dptm_small")
+        depthm_path = depthm_path.replace(".jpg", "_dpt.npy")
+        conf_path = depthm_path.replace("_dpt.npy", "_conf.npy")
+        dptm = np.load(depthm_path).astype(np.float32)
+        conf = np.load(conf_path).astype(np.float32)
+        
+  
+        if resize_flag:
+            dptm = Image.fromarray(dptm)
+            dptm = dptm.resize((reso[1], reso[0]), Image.BILINEAR)
+            dptm = np.array(dptm)
+            conf = Image.fromarray(conf)
+            conf = conf.resize((reso[1], reso[0]), Image.BILINEAR)
+            conf = np.array(conf)
+        depths_m.append(dptm)
+        confs_m.append(conf)
+
+    imgs = torch.from_numpy(np.stack(imgs, axis=0)).permute(0, 3, 1, 2).float() / 255.0  # [v c h w]-->[6,3,H,W]
+    depths = torch.from_numpy(np.stack(depths, axis=0)).float()  # [v h w] ---->[6,H,W]
+    depths_m = torch.from_numpy(np.stack(depths_m, axis=0)).float()  # [v h w] ---->[6,H,W]
+    confs_m = torch.from_numpy(np.stack(confs_m, axis=0)).float()  # [v h w]  ---->[6,H,W]
+    cks = torch.as_tensor(cks, dtype=torch.float32)
+
+    return imgs, depths, depths_m, confs_m, cks
+
+def load_lidar_info(info):
+    pcd_path = info["data_path"]
+    lidar2sensor = np.eye(4)
+    rot = info["sensor2lidar_rotation"]
+    trans = info["sensor2lidar_translation"]
+    lidar2sensor[:3, :3] = copy.deepcopy(rot.T)
+    lidar2sensor[:3, 3:4] = -1 * np.matmul(rot.T, trans.reshape(3, 1))
+    return pcd_path, lidar2sensor
