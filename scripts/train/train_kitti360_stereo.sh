844168b60a74e64c985cc3dff261846900cfffe1
MagicBoomLiu4GPUs <zliu@ok.sc.e.titech.ac.jp>
2025-05-12 21:45:32 +0900

    add

--- scripts/train/train_kitti360_stereo.sh
+++ scripts/train/train_kitti360_stereo.sh
@@ -2,11 +2,14 @@ TRAIN_KITTI360_OmniScene(){
 cd ../..
 cd codes
 configs_path="/home/zliu/Project2025/FeedStereoGS/codes/configs/OmniScene/omni_gs_kitti360_stereo_r50_224x840.py"
-work_dir="/home/zliu/Project2025/FeedStereoGS/outputs/omni_gs_kitti360_novelview_r50_224x840"
+work_dir="/data1/zliu/feedforward_outputs/omni_gs_kitti360_novelview_r50_224x840"
 resume_from="None"
 
+#configs 
+# - Single GPU YAML: accelerate_config_singleGPU.yaml
+# - Multi GPUs YAML: accelerate_config.yaml
 
-CUDA_VISIBLE_DEVICES=1 accelerate launch --config-file accelerate_config_singleGPU.yaml train_kitti360_stereo.py \
+CUDA_VISIBLE_DEVICES=1,2,3 accelerate launch --config-file accelerate_config.yaml train_kitti360_stereo.py \
     --py-config $configs_path \
     --work-dir  $work_dir \
     --resume-from $resume_from
