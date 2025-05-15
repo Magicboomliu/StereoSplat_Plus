dd63a2e91d3e92c23bbff9889ab3139e1bb20083
Zihua Liu <zliu@ok.sc.e.titech.ac.jp>
2025-05-15 21:45:13 +0900

    add

--- scripts/eval_and_vis/visualization/render_video.sh
+++ scripts/eval_and_vis/visualization/render_video.sh
@@ -2,8 +2,8 @@ Render_KITTI360_OmniScene_Render_Videos(){
 cd ../../..
 cd codes/validation
 configs_path="/home/zliu/Desktop/Project2025/FeedStereoGS/codes/configs/OmniScene/omni_gs_kitti360_stereo_r50_224x840.py"
-output_dir="/home/zliu/Desktop/Project2025/FeedStereoGS/temp/feedstereo_outputs/omni_gs_kitti360_novelview_r50_224x840/render_videos"
-load_from="/media/zliu/data12/outputs/omni_gs_kitti360_novelview_r50_224x840/omni_gs_kitti360_stereo_r50_224x804/pretrain/checkpoint-12000/"
+output_dir="/home/zliu/Desktop/Project2025/FeedStereoGS/temp/feedstereo_outputs/checkpoints33000/omni_gs_kitti360_novelview_r50_224x840/render_videos"
+load_from="/media/zliu/data12/outputs/omni_gs_kitti360_novelview_r50_224x840/omni_gs_kitti360_stereo_r50_224x804/pretrain/checkpoint-33000/"
 validation_list="None"
 #configs 
 # - Single GPU YAML: accelerate_config_singleGPU.yaml
--- scripts/eval_and_vis/visualization/rendered_FLC_views.sh
+++ scripts/eval_and_vis/visualization/rendered_FLC_views.sh
@@ -2,9 +2,10 @@ Render_KITTI360_OmniScene_LFC_Views(){
 cd ../../..
 cd codes/validation
 configs_path="/home/zliu/Desktop/Project2025/FeedStereoGS/codes/configs/OmniScene/omni_gs_kitti360_stereo_r50_224x840.py"
-output_dir="/home/zliu/Desktop/Project2025/FeedStereoGS/temp/feedstereo_outputs/omni_gs_kitti360_novelview_r50_224x840"
-load_from="/media/zliu/data12/outputs/omni_gs_kitti360_novelview_r50_224x840/omni_gs_kitti360_stereo_r50_224x804/pretrain/checkpoint-12000/"
+output_dir="/home/zliu/Desktop/Project2025/FeedStereoGS/temp/feedstereo_outputs/checkpoints33000/omni_gs_kitti360_novelview_r50_224x840"
+load_from="/media/zliu/data12/outputs/omni_gs_kitti360_novelview_r50_224x840/omni_gs_kitti360_stereo_r50_224x804/pretrain/checkpoint-33000/"
 validation_list="/home/zliu/Desktop/Project2025/FeedStereoGS/filenames/kitti360/trainval/val_for.txt"
+
 #configs 
 # - Single GPU YAML: accelerate_config_singleGPU.yaml
 # - Multi GPUs YAML: accelerate_config.yaml
@@ -13,7 +14,7 @@ TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 accelerate la
     --py-config $configs_path \
     --output_dir  $output_dir \
     --load_from $load_from \
-    --validation_list $validation_list
+    --validation_list $validation_list 
     # --output_vis
 
 }
