213d59ad6b2789b279391b09c21c0256e3897b83
MagicBoomLiu4GPUs <zliu@ok.sc.e.titech.ac.jp>
2025-05-16 18:02:59 +0900

    add local to server

--- scripts/train/train_kitti360_stereo.sh
+++ scripts/train/train_kitti360_stereo.sh
@@ -1,15 +1,15 @@
 TRAIN_KITTI360_OmniScene(){
 cd ../..
 cd codes
-configs_path="/home/zliu/Desktop/Project2025/FeedStereoGS/codes/configs/OmniScene/omni_gs_kitti360_stereo_r50_224x840.py"
-work_dir="/media/zliu/data12/dataset/KITTI/VSRD_Format/feedstereo_outputs/omni_gs_kitti360_novelview_r50_224x840"
-resume_from="/media/zliu/data12/outputs/omni_gs_kitti360_novelview_r50_224x840/omni_gs_kitti360_stereo_r50_224x804/pretrain/checkpoint-12000/"
+configs_path="/home/zliu/Project2025/Feedforward_Based_3DGS/more_supp_vanilla/FeedStereoGS/codes/configs/OmniScene/omni_gs_kitti360_stereo_r50_224x840.py"
+work_dir="/data1/zliu/feedforward_outputs/more_vis_for_vis/omni_gs_kitti360_novelview_r50_224x840"
+resume_from="/data1/zliu/feedforward_outputs/omni_gs_kitti360_novelview_r50_224x840/latest/"
 
 #configs 
 # - Single GPU YAML: accelerate_config_singleGPU.yaml
 # - Multi GPUs YAML: accelerate_config.yaml
 
-TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 accelerate launch train_kitti360_stereo.py \
+TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --config-file accelerate_config.yaml train_kitti360_stereo.py \
     --py-config $configs_path \
     --work-dir  $work_dir \
     --resume-from $resume_from
@@ -17,4 +17,3 @@ TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 accelerate la
 }
 
 TRAIN_KITTI360_OmniScene
-
