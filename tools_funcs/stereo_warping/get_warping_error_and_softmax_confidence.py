508e4e083a3a2f98583b50e6e53390eca232f713
Zihua Liu <zliu@ok.sc.e.titech.ac.jp>
2025-05-22 16:41:30 +0900

    add

--- tools_funcs/stereo_warping/get_warping_error_and_softmax_confidence.py
+++ tools_funcs/stereo_warping/get_warping_error_and_softmax_confidence.py
@@ -7,7 +7,7 @@ import cv2
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import matplotlib.pyplot as plt
 
 def load_the_Metric3DV2_Results(path,scale=256):
     # Read the image in unchanged mode (preserves uint16 format)
@@ -56,11 +56,49 @@ def get_conf_and_warped_error(left_image_data,right_image_data,metric_depth,foca
     return error, conf
 
 
+
+def loaded_projected_sparse_depth_and_valid_mask(path):
+    depth = np.array(cv2.imread(path, cv2.IMREAD_UNCHANGED)).astype(np.float32)/256
+
+    mask1 = depth>0
+    mask2 = depth<=80
+    
+    mask1 = mask1.astype(np.float32)
+    mask2 = mask2.astype(np.float32)
+    
+    mask = mask1 * mask2
+    
+    return depth, mask
+
+
+def compute_depth_errors(gt_depth: np.ndarray, aligned_depth: np.ndarray, valid_mask: np.ndarray):
+    """
+    Compute MSE and MAE between GT and aligned prediction in valid regions.
+
+    Args:
+        gt_depth (np.ndarray): Ground truth depth map, shape [H, W] or [N].
+        aligned_depth (np.ndarray): Aligned predicted depth map, same shape as gt_depth.
+        valid_mask (np.ndarray): Boolean mask of valid pixels, same shape.
+
+    Returns:
+        mse (float): Mean squared error.
+        mae (float): Mean absolute error.
+    """
+    gt_valid = gt_depth[valid_mask]
+    pred_valid = aligned_depth[valid_mask]
+
+    mse = np.mean((gt_valid - pred_valid) ** 2)
+    mae = np.mean(np.abs(gt_valid - pred_valid))
+
+    return mse, mae
+
 if __name__=="__main__":
     
-    left_image = "/data1/StereoDatasets/KITTI/KITTI360/data_2d_raw/2013_05_28_drive_0000_sync/image_00/data_rect/0000000259.png"
+    left_image = "/media/zliu/data12/dataset/KITTI/KITTI360/image_data/data_2d_raw/2013_05_28_drive_0000_sync/image_00/data_rect/0000000259.png"
     right_image = left_image.replace("image_00","image_01")
-    metric_depth = left_image.replace("/data1/StereoDatasets/KITTI/KITTI360/","/data1/StereoDatasets/KITTI/KITTI360/monocular_depth/Metric3DV2/").replace(os.path.basename(left_image)[:-4],"{}_dpt".format(os.path.basename(left_image)[:-4]))
+    metric_depth = "/media/zliu/data12/dataset/KITTI/KITTI360/PseudoDepth_NMRFStereo/data_2d_raw/2013_05_28_drive_0000_sync/image_00/data_rect/0000000259.png"
+    
+    gt_depth = "/media/zliu/data12/dataset/KITTI/VSRD_Format/projected_sparse_lidar/data_2d_raw/2013_05_28_drive_0000_sync/image_00/data_rect/0000000259.png"
     
     assert os.path.exists(left_image)
     assert os.path.exists(right_image)
@@ -70,15 +108,18 @@ if __name__=="__main__":
     left_image_data = loaded_image(left_image)
     right_image_data = loaded_image(right_image)
     metric_depth = load_the_Metric3DV2_Results(metric_depth)
-  
+    
+    gt_depth_sparse,gt_mask = loaded_projected_sparse_depth_and_valid_mask(gt_depth)
+    
+    mse, mae = compute_depth_errors(gt_depth=gt_depth_sparse,aligned_depth=metric_depth,
+                         valid_mask=gt_mask.astype(np.bool_))
+    
+
 
     error,conf= get_conf_and_warped_error(left_image_data,right_image_data,metric_depth,focal_length=552.554261,baseline=0.5941836995443964,
-                              temp=100)
+                              temp=200)
     
-    print(conf.shape)
-    print(conf.max())
-    print(conf.min())
-    print(conf.mean())
+    
         
     # import skimage.io 
     # warped_left_vis = warped_left.squeeze(0).permute(1,2,0).cpu().numpy().astype(np.uint8)
