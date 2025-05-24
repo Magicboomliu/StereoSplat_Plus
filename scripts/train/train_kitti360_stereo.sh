04b1d2f4c1f902dc488c0a81d9f9d4fe0e3891d6
MagicBoomLiu4GPUs <zliu@ok.sc.e.titech.ac.jp>
2025-05-24 23:50:40 +0900

    add

--- scripts/train/train_kitti360_stereo.sh
+++ scripts/train/train_kitti360_stereo.sh
@@ -3,13 +3,13 @@ cd ../..
 cd codes
 configs_path="/home/zliu/Project2025/Feedforward_Based_3DGS/more_supp_vanilla/FeedStereoGS/codes/configs/OmniScene/omni_gs_kitti360_stereo_r50_224x840_supp6_adaptive_input_frame.py"
 work_dir="/data1/zliu/feedforward_outputs/Vanilla_Omni_Scene/Vanilla_Omni_Scene_Adaptive_Input_NMRFStereo/omni_gs_kitti360_novelview_r50_224x840"
-resume_from="None"
+resume_from="/data1/zliu/feedforward_outputs/Vanilla_Omni_Scene/Vanilla_Omni_Scene_Adaptive_Input_NMRFStereo/omni_gs_kitti360_novelview_r50_224x840/checkpoint-3000/"
 
 #configs 
 # - Single GPU YAML: accelerate_config_singleGPU.yaml
 # - Multi GPUs YAML: accelerate_config.yaml
 
-TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --config-file accelerate_config.yaml train_kitti360_stereo.py \
+TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=1,2,3 accelerate launch --config-file accelerate_config.yaml train_kitti360_stereo.py \
     --py-config $configs_path \
     --work-dir  $work_dir \
     --resume-from $resume_from
