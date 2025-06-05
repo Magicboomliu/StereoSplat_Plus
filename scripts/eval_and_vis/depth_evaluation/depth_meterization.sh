2d8336495c96f6e9ca69a27d071cbd9efd4d471d
Zihua Liu <zliu@ok.sc.e.titech.ac.jp>
2025-06-05 14:51:16 +0900

    organzing the coding structures

---
+++ scripts/eval_and_vis/depth_evaluation/depth_meterization.sh
@@ -0,0 +1,9 @@
+Evaluate_The_Depth_Of_Unimatch(){
+
+
+
+}
+
+
+
+Evaluate_The_Depth_Of_Unimatch
--- scripts/eval_and_vis/visualization/render_video.sh
+++ scripts/eval_and_vis/visualization/render_video.sh
@@ -3,12 +3,12 @@ cd ../../..
 cd codes/validation
 configs_path="/home/Desktop/Project2025/FeedStereoGS/codes/configs/OmniScene/eval/omniscene_vanilla_settings.py"
 output_dir="/data/feedforward_outputs/visualization_outputs/omniscene_vanilla_settings/rendered_videos"
-load_from="/data/feedforward_outputs/checkpoint-48000/"
+load_from="/data/feedforward_outputs/output_models/OmniScene/"
 validation_list="/home/Desktop/Project2025/FeedStereoGS/filenames/kitti360/trainval/demo.txt"
 # - Single GPU YAML: accelerate_config_singleGPU.yaml
 # - Multi GPUs YAML: accelerate_config.yaml
 
-TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 accelerate launch --config-file accelerate_config_singleGPU.yaml rendered_short_videos.py \
+TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 accelerate launch --config-file accelerate_config_singleGPU.yaml omniscene/rendered_short_videos.py \
     --py-config $configs_path \
     --output_dir  $output_dir \
     --load_from $load_from \
