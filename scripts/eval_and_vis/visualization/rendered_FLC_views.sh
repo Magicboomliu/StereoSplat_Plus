3a887c8932ce26aa4d39293113f7b7f46a8a5abc
Zihua Liu <zliu@ok.sc.e.titech.ac.jp>
2025-06-05 14:51:16 +0900

    organzing the coding structures

--- scripts/eval_and_vis/visualization/rendered_FLC_views.sh
+++ scripts/eval_and_vis/visualization/rendered_FLC_views.sh
@@ -3,7 +3,7 @@ cd ../../..
 cd codes/validation
 configs_path="/home/Desktop/Project2025/FeedStereoGS/codes/configs/OmniScene/eval/omniscene_vanilla_settings.py"
 output_dir="/data/feedforward_outputs/visualization_outputs/omniscene_vanilla_settings"
-load_from="/data/feedforward_outputs/checkpoint-48000/"
+load_from="/data/feedforward_outputs/output_models/OmniScene/"
 validation_list="/home/Desktop/Project2025/FeedStereoGS/filenames/kitti360/trainval/demo.txt"
 
 # demo.txt
@@ -13,7 +13,7 @@ validation_list="/home/Desktop/Project2025/FeedStereoGS/filenames/kitti360/train
 # - Single GPU YAML: accelerate_config_singleGPU.yaml
 # - Multi GPUs YAML: accelerate_config.yaml
 
-TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 accelerate launch --config-file accelerate_config_singleGPU.yaml rendered_FLC_view_and_depth.py \
+TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 accelerate launch --config-file accelerate_config_singleGPU.yaml omniscene/rendered_FLC_view_and_depth.py \
     --py-config $configs_path \
     --output_dir  $output_dir \
     --load_from $load_from \
