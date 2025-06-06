1e8b3b31d10372a9e50fa78d9f2187cdf2c86293
Zihua Liu <zliu@ok.sc.e.titech.ac.jp>
2025-06-06 15:56:46 +0900

    add depthsplat trainining loss

--- codes/depthsplat/src/models/model_warpper_splat.py
+++ codes/depthsplat/src/models/model_warpper_splat.py
@@ -24,9 +24,10 @@ from torch import Tensor, nn
 # Decoder Here
 from .decoder.decoder_splatting_head_cuda import DecoderSplattingCUDA
 from safetensors.torch import load_file
-
 # vis here
 import matplotlib.pyplot as plt
+from .rgb_loss import LPIPS
+from .utils import maybe_resize
 
 
 def compute_depth_mae_mse(depth_pred, depth_gt, valid_min=0.0, valid_max=150.0):
@@ -63,7 +64,6 @@ def compute_depth_mae_mse(depth_pred, depth_gt, valid_min=0.0, valid_max=150.0):
     return mae, mse
 
 
-
 @dataclass
 class OptimizerCfg:
     lr: float
@@ -98,6 +98,11 @@ class ModelWarpper(nn.Module):
             }
             self.depth_estimator.load_state_dict(stripped_state_dict, strict=True)
             print("depth branch initailzation with {}".format(self.unimatch_weight))
+        
+        # preception loss here
+        self.perceptual_loss = LPIPS().eval()
+        
+        
         
          
     
@@ -158,10 +163,13 @@ class ModelWarpper(nn.Module):
         output_batch_dict["output_c2ws"] = batch["outputs"]["c2w"].to(device_id, dtype=self.dtype)
         output_batch_dict["output_fovxs"] = batch["outputs"]["fovx"].to(device_id, dtype=self.dtype)
         output_batch_dict["output_fovys"] = batch["outputs"]["fovy"].to(device_id, dtype=self.dtype)
+        output_batch_dict['output_sparse_depth'] = batch['outputs']['sparse_gt_depth'].to(device_id, dtype=self.dtype)
         
 
     
         return input_batch_dict,output_batch_dict
+    
+    
     
 
     def forward(self,batch, mode="train", iter=0, cfg=None):
@@ -235,36 +243,183 @@ class ModelWarpper(nn.Module):
         
         #first 2 dimension is the novel final ,final dimension is the input view
         render_c2w = output_batch_dict["output_c2ws"] #(1,6,4,4)
+        output_intrinsics = intrinsics[:,0:1,:,:].repeat(1,render_c2w.shape[1],1,1)
+        z_near_batch = torch.from_numpy(np.array([cfg.near])).unsqueeze(0).repeat(render_c2w.shape[0],render_c2w.shape[1]).type_as(render_c2w)
+        z_far_batch = torch.from_numpy(np.array([cfg.far])).unsqueeze(0).repeat(render_c2w.shape[0],render_c2w.shape[1]).type_as(render_c2w)
+
         
         
-        z_near_batch = torch.from_numpy(np.array([cfg.near])).unsqueeze(0).repeat(input_images.shape[0],input_images.shape[1]).type_as(input_images)
-        z_far_batch = torch.from_numpy(np.array([cfg.far])).unsqueeze(0).repeat(input_images.shape[0],input_images.shape[1]).type_as(input_images)
-    
         rendered_results = self.decoder_branch(gaussians=estimated_raw_gaussains_dict["gaussians"],
                                                extrinsics= render_c2w,
-                                               intrinsics = intrinsics,
+                                               intrinsics = output_intrinsics,
                                                near = z_near_batch,
                                                far = z_far_batch,
                                                image_shape=(height,width),
                                                depth_mode = 'depth'
                                                )
 
-        rendered_color = rendered_results['color'] # torch.Size([1, 2, 3, 224, 832])
-        rendered_depth = rendered_results['depth'] # torch.Size([1, 2, 1, 224, 832])
-        rendered_alpha = rendered_results['alpha'] # torch.Size([1, 2, 1, 224, 832])
+        rendered_color = rendered_results['color'] # torch.Size([1, V, 3, 224, 832])
+        rendered_depth = rendered_results['depth'] # torch.Size([1, V, 1, 224, 832])
+        rendered_alpha = rendered_results['alpha'] # torch.Size([1, V, 1, 224, 832])
         
-        print(rendered_depth.shape)
-        print(rendered_color.shape)
-        print(rendered_alpha.shape)
-        
-        quit()
-        
-        # rendered_color = rendered_results['color'] # torch.Size([1, 2, 3, 224, 832])
-        # rendered_depth = rendered_results['depth'] # torch.Size([1, 2, 1, 224, 832])
-        # rendered_alpha = rendered_results['alpha'] # torch.Size([1, 2, 1, 224, 832])
+        rendered_depth = rendered_depth.squeeze(2)
+        rendered_alpha = rendered_alpha.squeeze(2)
         
         
-        # calculate the loss here
+        if mode=='train':
+    
+            # loss here
+            # dict_keys(['output_imgs', 'output_depths', 'output_depths_m', 'output_confs_m', 
+                        # 'output_positions', 'output_rays_o', 
+                        # 'output_rays_d', 'output_c2ws', 
+                        # 'output_fovxs', 'output_fovys'])
+
+            # Get the Loss Here
+            # ======================== losses ======================== #
+            loss = 0.0
+            loss_terms = {}
+            def set_loss(key, split, loss_value, loss_weight=1.0):
+                loss_terms[f"{split}/loss_{key}"] = loss_value.item()
+                loss_terms[f"{split}/loss_{key}_w"] = loss_value.item() * loss_weight
+            
+            output_rgb = output_batch_dict['output_imgs']
+            pseudo_depth_gt = output_batch_dict['output_depths_m']
+            sparse_depth_gt = output_batch_dict['output_sparse_depth']
+            
+            # RGB Loss Here
+            if cfg.loss_settings_dict.rendered_rgb_supervision:
+                rgb_loss_total = 0
+                if cfg.loss_settings_dict.rendered_rgb_supervison_type=="MSE":
+                    rec_loss = ((output_rgb - rendered_color) ** 2).mean()
+                    rgb_loss_total = rec_loss *1.0
+                elif cfg.loss_settings_dict.rendered_rgb_supervison_type=="MSE_LPIPS":
+                    rec_loss = F.mse_loss(output_rgb,rendered_color)
+                    # preception loss
+                    current_height, current_width = output_rgb.shape[-2:]
+                    
+                    preception_loss = self.perceptual_loss(output_rgb.reshape(-1,3,current_height,current_width),
+                                                        rendered_color.reshape(-1,3,current_height,current_width)
+                                                        )
+                    preception_loss = preception_loss.mean()
+                    
+                    lpips_loss_alpha = cfg.loss_settings_dict.lpips_alpha
+                    
+                    rgb_loss_total = rec_loss + lpips_loss_alpha * preception_loss
+                
+                
+                loss +=rgb_loss_total
+                
+                set_loss(key='rgb_loss',split=mode,loss_value=rgb_loss_total,
+                                                    loss_weight=1.0)
+            
+            # rendered depth loss here
+            if cfg.loss_settings_dict.rendered_depth_supervision:
+                if cfg.loss_settings_dict.rendered_depth_supervision_type =='sparse_gt':
+                    valid_mask_01 = sparse_depth_gt>0
+                    valid_mask_02 = sparse_depth_gt<120
+                    valid_mask = valid_mask_01 * valid_mask_02
+                    valid_mask = valid_mask.bool()
+                    
+                    sparse_depth_loss = F.l1_loss(sparse_depth_gt[valid_mask],rendered_depth[valid_mask])
+                    sparse_depth_loss = sparse_depth_loss * cfg.loss_settings_dict.rendered_depth_weight
+
+                    depth_loss = sparse_depth_loss
+                    
+                elif cfg.loss_settings_dict.rendered_depth_supervision_type =='pseudo':
+                    
+                    valid_mask_01 = pseudo_depth_gt>0
+                    valid_mask_02 = pseudo_depth_gt<120
+                    valid_mask = valid_mask_01 * valid_mask_02
+                    valid_mask = valid_mask.bool()
+                    
+                    pseudo_depth_loss = F.l1_loss(pseudo_depth_gt[valid_mask],rendered_depth[valid_mask])
+                    pseudo_depth_loss = pseudo_depth_loss * cfg.loss_settings_dict.rendered_depth_weight
+                    
+                    depth_loss = pseudo_depth_loss
+                    
+                elif cfg.loss_settings_dict.rendered_depth_supervision_type =='sparse_gt_pseudo':
+                    valid_mask_01 = sparse_depth_gt>0
+                    valid_mask_02 = sparse_depth_gt<120
+                    valid_mask = valid_mask_01 * valid_mask_02
+                    valid_mask = valid_mask.bool()
+                    
+                    sparse_depth_loss = F.l1_loss(sparse_depth_gt[valid_mask],rendered_depth[valid_mask])
+                    sparse_depth_loss = sparse_depth_loss * cfg.loss_settings_dict.rendered_depth_weight
+
+
+                    valid_mask_01 = pseudo_depth_gt>0
+                    valid_mask_02 = pseudo_depth_gt<120
+                    valid_mask = valid_mask_01 * valid_mask_02
+                    valid_mask = valid_mask.bool()
+                    
+                    pseudo_depth_loss = F.l1_loss(pseudo_depth_gt[valid_mask],rendered_depth[valid_mask])
+                    pseudo_depth_loss = pseudo_depth_loss * cfg.loss_settings_dict.rendered_depth_weight
+
+                    depth_loss = (sparse_depth_loss + pseudo_depth_loss) *1.0/2.0
+                    
+                else:
+                    raise NotImplementedError
+
+                loss +=depth_loss
+                set_loss(key='rendered_depth_loss',split=mode,loss_value=depth_loss,
+                                                    loss_weight=cfg.loss_settings_dict.rendered_depth_weight)
+            
+            
+            # depth estimation loss here
+            if cfg.loss_settings_dict.depth_estimator_supervision:
+                # everything is OK
+                if cfg.loss_settings_dict.depth_estimator_suppervision_type =='sparse_gt':
+                    pred_depth = results_dict['depth_preds'][0]
+                    valid_mask_01 = input_sparse_gt_depth>0
+                    valid_mask_02 = input_sparse_gt_depth<120
+                    valid_mask = valid_mask_01 * valid_mask_02
+                    valid_mask = valid_mask.bool()
+                    sparse_depth_estimation_loss = F.l1_loss(input_sparse_gt_depth[valid_mask],pred_depth[valid_mask])            
+                    depth_estimation_loss = sparse_depth_estimation_loss * cfg.loss_settings_dict.depth_estimation_weight
+                
+                elif cfg.loss_settings_dict.depth_estimator_suppervision_type =='pseudo':
+                    
+                    pred_depth = results_dict['depth_preds'][0]
+                    valid_mask_01 = input_pseudo_depth>0
+                    valid_mask_02 = input_pseudo_depth<120
+                    valid_mask = valid_mask_01 * valid_mask_02
+                    valid_mask = valid_mask.bool()
+                    pseudo_depth_estimation_loss = F.l1_loss(input_pseudo_depth[valid_mask],pred_depth[valid_mask])            
+                    depth_estimation_loss = pseudo_depth_estimation_loss * cfg.loss_settings_dict.depth_estimation_weight
+
+                
+                elif cfg.loss_settings_dict.depth_estimator_suppervision_type =='sparse_gt_pseudo':
+                    
+                    pred_depth = results_dict['depth_preds'][0]
+                    valid_mask_01 = input_pseudo_depth>0
+                    valid_mask_02 = input_pseudo_depth<120
+                    valid_mask = valid_mask_01 * valid_mask_02
+                    valid_mask = valid_mask.bool()
+                    pseudo_depth_estimation_loss = F.l1_loss(input_pseudo_depth[valid_mask],pred_depth[valid_mask])            
+                    
+
+                    valid_mask_01 = input_sparse_gt_depth>0
+                    valid_mask_02 = input_sparse_gt_depth<120
+                    valid_mask = valid_mask_01 * valid_mask_02
+                    valid_mask = valid_mask.bool()
+                    sparse_depth_estimation_loss = F.l1_loss(input_sparse_gt_depth[valid_mask],pred_depth[valid_mask])  
+
+                    
+                    depth_estimation_loss = 0.5 *(pseudo_depth_estimation_loss+sparse_depth_estimation_loss) * cfg.loss_settings_dict.depth_estimation_weight
+                    
+
+                loss +=depth_estimation_loss
+                set_loss(key='depth_estimation_loss',split=mode,loss_value=depth_estimation_loss,
+                                                    loss_weight=cfg.loss_settings_dict.depth_estimation_weight)
+
+            
+            return loss, loss_terms,rendered_color,rendered_depth,rendered_alpha,estimated_raw_gaussains_dict
+
+        elif mode=='val' or mode=='test':
+            
+            return rendered_color,rendered_depth,rendered_alpha,estimated_raw_gaussains_dict
+            
+        
         
 
         
