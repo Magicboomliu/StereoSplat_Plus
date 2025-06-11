e13bcdea9412f342317518721f870162cc7a9389
MagicBoomLiu4GPUs <zliu@ok.sc.e.titech.ac.jp>
2025-06-11 14:25:11 +0900

    add

--- codes/configs/OmniScene/omniscene_vanilla_settings.py
+++ codes/configs/OmniScene/omniscene_vanilla_settings.py
@@ -47,7 +47,7 @@ test_filelist="/home/zliu/Project2025/FeedStereoGS/filenames/kitti360/trainval/v
 sequence='2013_05_28_drive_0000_sync'
 data_version="bin_infos_8.0"
 supp_view_nums=3
-camera_model='OpenCV' # select from OpenCV and OpenGL
+camera_model='OpenGL' # select from OpenCV and OpenGL
 
 
 depth_info_params = dict(
