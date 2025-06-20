4b2ce61c63b6e1c9f834867a8be8389b5ff73c40
MagicBoomLiu4GPUs <zliu@ok.sc.e.titech.ac.jp>
2025-06-20 17:34:19 +0900

    add

--- codes/configs/DepthSplat/depthsplat_gs_revised_config.py
+++ codes/configs/DepthSplat/depthsplat_gs_revised_config.py
@@ -6,7 +6,7 @@ _base_ = [
 # exp name
 # output directionary
 exp_name = "depthsplat_kitti360_stereo_224x840"
-output_dir = "/data1/zliu/feedforward_outputs/depthsplat_gs_revised/outputs_vis/{}".format(exp_name)
+output_dir = "/data1/zliu/feedforward_outputs/depthsplat_revised/outputs_vis/{}".format(exp_name)
 validation_vis_progress=True
 
 
