b82bd147e31359744bddd82d7eff71a4f1d069c2
MagicBoomLiu4GPUs <zliu@ok.sc.e.titech.ac.jp>
2025-06-23 20:38:38 +0900

    add

---
+++ scripts/gaussain_fusion/gaussain_fusion.sh
@@ -0,0 +1,28 @@
+GaussainFusion_Inside_BIN(){
+cd ../..
+
+cd codes/playrgound/
+
+
+configs_path="/home/zliu/Project2025/FeedStereoGS/codes/configs/OmniScene/eval/omniscene_vanilla_settings.py"
+output_dir="/data1/zliu/temp_for_0617/Vis_And_Evals/Omniscene/Metric3DV2/"
+load_from="/data1/zliu/temp_for_0617/OmniScene/Metric3Dv2/checkpoint-45000/"
+validation_list="/home/zliu/Project2025/FeedStereoGS/filenames/kitti360/trainval/demo.txt"
+
+# demo.txt
+# val_2013_05_28_drive_0000_sync_complete.txt
+
+#configs 
+# - Single GPU YAML: accelerate_config_singleGPU.yaml
+# - Multi GPUs YAML: accelerate_config.yaml
+
+TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 accelerate launch --config-file accelerate_config_singleGPU.yaml GaussainFusion/inside_bin_gaussain_fusion.py \
+    --py-config $configs_path \
+    --output_dir  $output_dir \
+    --load_from $load_from \
+    --validation_list $validation_list \
+    --output_vis
+
+}
+
+GaussainFusion_Inside_BIN
