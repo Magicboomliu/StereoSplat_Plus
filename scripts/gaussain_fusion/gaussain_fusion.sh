9030c09e41459ec250992622a556ee9c66d31381
MagicBoomLiu4GPUs <zliu@ok.sc.e.titech.ac.jp>
2025-06-24 18:12:54 +0900

    add

--- scripts/gaussain_fusion/gaussain_fusion.sh
+++ scripts/gaussain_fusion/gaussain_fusion.sh
@@ -1,28 +1,23 @@
-GaussainFusion_Inside_BIN(){
+GaussainFusion_Inside_BIN_DepthSplat(){
 cd ../..
-
 cd codes/playrgound/
 
-
-configs_path="/home/zliu/Project2025/FeedStereoGS/codes/configs/OmniScene/eval/omniscene_vanilla_settings.py"
-output_dir="/data1/zliu/temp_for_0617/Vis_And_Evals/Omniscene/Metric3DV2/"
-load_from="/data1/zliu/temp_for_0617/OmniScene/Metric3Dv2/checkpoint-45000/"
-validation_list="/home/zliu/Project2025/FeedStereoGS/filenames/kitti360/trainval/demo.txt"
+configs_path="/home/zliu/Project2025/FeedStereoGS/codes/configs/DepthSplat/depthsplat_gs_revised.py"
+work_dir="/data1/zliu/temp_for_0617/Vis_And_Evals/GaussainFusion/DepthSplat/BIN_INSIDE"
+resume_from="/data1/zliu/temp_for_0617/DepthSplat/SH0_Version/checkpoint-99000/"
+val_filelist="/home/zliu/Project2025/FeedStereoGS/filenames/kitti360/trainval/demo.txt"
 
 # demo.txt
 # val_2013_05_28_drive_0000_sync_complete.txt
-
 #configs 
 # - Single GPU YAML: accelerate_config_singleGPU.yaml
 # - Multi GPUs YAML: accelerate_config.yaml
 
-TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 accelerate launch --config-file accelerate_config_singleGPU.yaml GaussainFusion/inside_bin_gaussain_fusion.py \
+TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 accelerate launch --config-file accelerate_config_singleGPU.yaml GaussainFusion/inside_bin_gaussain_fusion_depthsplatSH0.py \
     --py-config $configs_path \
-    --output_dir  $output_dir \
-    --load_from $load_from \
-    --validation_list $validation_list \
-    --output_vis
-
+    --work-dir  $work_dir \
+    --resume-from $resume_from \
+    --val_filelist $val_filelist 
 }
 
-GaussainFusion_Inside_BIN
+GaussainFusion_Inside_BIN_DepthSplat
