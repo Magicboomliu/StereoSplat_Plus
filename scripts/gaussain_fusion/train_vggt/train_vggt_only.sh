1803df5ef144d20f810e346a2d6ed9214f84e377
MagicBoomLiu4GPUs <zliu@ok.sc.e.titech.ac.jp>
2025-07-02 16:05:43 +0900

    dd

--- scripts/gaussain_fusion/train_vggt/train_vggt_only.sh
+++ scripts/gaussain_fusion/train_vggt/train_vggt_only.sh
@@ -10,7 +10,7 @@ resume_from="None"
 # - Single GPU YAML: accelerate_config_singleGPU.yaml
 # - Multi GPUs YAML: accelerate_config.yaml
 export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:64
-TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 accelerate launch --config-file accelerate_config_singleGPU.yaml train_kitti360_vggt.py \
+TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 accelerate launch --config-file accelerate_config.yaml train_kitti360_vggt.py \
     --py-config $configs_path \
     --work-dir  $work_dir \
     --resume-from $resume_from
