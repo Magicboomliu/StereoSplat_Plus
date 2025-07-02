15f1d2dbf7c17bbbd28543dff9c02eb1ebf36db0
MagicBoomLiu4GPUs <zliu@ok.sc.e.titech.ac.jp>
2025-07-02 16:04:18 +0900

    add the vggt finetuning code on KITTI360

--- codes/KITTI360_Lab/VGGT/vggt/train_kitti360_vggt.py
+++ codes/KITTI360_Lab/VGGT/vggt/train_kitti360_vggt.py
@@ -30,6 +30,77 @@ import data.KITTI360_VGGT.dataloader as datasets
 import os
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
 import matplotlib.pyplot as plt
+import json
+
+
+def saved_into_json(data_dict,path):
+    with open(path, "w") as f:
+        json.dump(data_dict, f, indent=4)
+
+class Pose_Quality_Meter(object):
+    def __init__(self,rra,rta):
+        self.rra = rra
+        self.rta = rta
+        self.counter = 0
+        
+    def update(self,rra,rta):
+        self.rra +=rra
+        self.rta +=rta
+        self.counter = self.counter +1
+        
+    def get_stats(self):
+        if self.counter==0:
+            return {"rra":0,
+                    "rta":0}
+        else:
+            return {
+                "rra": self.rra * 1.0 / self.counter,
+                "rta": self.rta * 1.0 /self.counter
+            }
+
+class Depth_Quality_Meter(object):
+    def __init__(self,mae,mse):
+        self.mae = mae
+        self.mse = mse
+        self.counter =0
+    
+    def update(self,mae,mse):
+        self.mae +=mae
+        self.mse +=mse
+        self.counter +=1
+        
+    def get_stats(self):
+        if self.counter==0:
+            return {"mae":0,
+                    "mse":0}
+        else:
+            return {
+                "mae": self.mae * 1.0 / self.counter,
+                "mse": self.mse * 1.0 /self.counter
+            }
+
+class Pcd_Quality_Meter(object):
+    def __init__(self,mae,mse):
+        self.mae = mae
+        self.mse = mse
+        self.counter =0
+    
+    def update(self,mae,mse):
+        self.mae +=mae
+        self.mse +=mse
+        self.counter +=1
+        
+    def get_stats(self):
+        if self.counter==0:
+            return {"mae":0,
+                    "mse":0}
+        else:
+            return {
+                "mae": self.mae * 1.0 / self.counter,
+                "mse": self.mse * 1.0 /self.counter
+            }
+
+
 
 def create_logger(log_file=None, is_main_process=False, log_level=logging.INFO):
     if not is_main_process:
@@ -180,6 +251,7 @@ def main(args):
     if cfg.vggt_pretrained_weight!="None":
         # loaded the pretrained weight
         my_model.load_state_dict(torch.load(cfg.vggt_pretrained_weight),strict=False)
+        logging.info("Loading the VGGT Weight Successfully!")
 
 
 
@@ -259,34 +331,119 @@ def main(args):
                     #               'world_points', 
                     #               'world_points_conf', 'images'])
                     '''
-                    predictions,loss,loss_terms = my_model(batch,mode='train',cfg=cfg) 
+                    predictions,loss,logs = my_model.forward(batch,mode='train',cfg=cfg) 
+                else:
+                    predictions,loss,logs = my_model.module.forward(batch,mode='train',cfg=cfg)
                     
-                    print(loss_terms)
-   
-                    quit()
-                    
-                    # pred_depths = preditions['depth']
-                    
-                    # plt.subplot(2,2,1)
-                    # plt.axis("off")
-                    # plt.imshow(pred_depths[0][0].squeeze(-1).detach().cpu().numpy())
-                    # plt.subplot(2,2,2)
-                    # plt.axis("off")
-                    # plt.imshow(pred_depths[0][1].squeeze(-1).detach().cpu().numpy())  
-                    # plt.subplot(2,2,3)
-                    # plt.axis("off")
-                    # plt.imshow(pred_depths[0][2].squeeze(-1).detach().cpu().numpy())  
-                    # plt.subplot(2,2,4)
-                    # plt.axis("off")
-                    # plt.imshow(pred_depths[0][3].squeeze(-1).detach().cpu().numpy()) 
-                    # plt.savefig("1.png")
-                    # print(pred_depths.shape)
-                    # print(preditions.keys())
-                    # quit()
+                loss = torch.nan_to_num(loss, nan=0.0, posinf=0.0, neginf=0.0)
+                if torch.isnan(loss) or torch.isinf(loss):
+                    print(f"[Warning] NaN or INF loss at iter {global_iter}, skipping...")
+                    continue  # 跳过当前 batch
 
-    
+                with torch.autograd.detect_anomaly():
+                    accelerator.backward(loss)
+
+                if accelerator.sync_gradients:
+                    grad_norm = accelerator.clip_grad_norm_(my_model.parameters(), cfg.grad_max_norm)
+                
+                optimizer.step()
+                scheduler.step()
 
 
+            accelerator.wait_for_everyone()
+            if accelerator.sync_gradients and accelerator.is_main_process:
+                if global_iter % cfg.save_freq == 0:
+                    if accelerator.is_main_process:
+                        save_file_name = os.path.join(os.path.abspath(args.work_dir), f'checkpoint-{global_iter}')
+                        accelerator.save_state(save_file_name)
+                        dst_file = osp.join(args.work_dir, 'latest')
+                        mmengine.utils.symlink(save_file_name, dst_file)
+                        if logger is not None:
+                            logger.info('[TRAIN] Save latest state dict to {}.'.format(save_file_name))
+
+            if global_iter % 100 == 0:
+                torch.cuda.empty_cache()
+                
+                
+            if global_iter % cfg.val_freq == 0:
+                my_model.eval()
+                if accelerator.is_main_process:
+                    pose_meter = Pose_Quality_Meter(rra=0.0,rta=0.0)
+                    depth_meter = Depth_Quality_Meter(mae=0.0,mse=0.0)
+                    pcd_meter = Pcd_Quality_Meter(mae=0.0,mse=0.0)
+
+
+                    for i_iter_val, batch_val in enumerate(val_dataloader):
+                        print("Processed {}/{}".format(i_iter_val,len(val_dataloader)))
+                        overall_val_batch_save_dir = osp.join(cfg.output_dir, cfg.exp_name, "validation",
+                                                                "step-{}".format(global_iter))
+                        os.makedirs(overall_val_batch_save_dir,exist_ok=True)
+                        val_batch_save_dir = os.path.join(overall_val_batch_save_dir,"batch-{}".format(i_iter_val))
+                        os.makedirs(val_batch_save_dir,exist_ok=True)
+
+                        if args.gpus<=1:
+                            # forward here 
+                            # get the psnr, ssim, mae and mse as well as the saved the visualization results
+                            predictions,loss,loss_terms,input_dict,output_rgb_meter_dict = my_model.validation_step(batch_val, val_batch_save_dir,cfg)
+                        else:
+                            predictions,loss,loss_terms,input_dict,output_rgb_meter_dict = my_model.module.validation_step(batch_val, val_batch_save_dir,cfg)
+                
+
+                        pose_meter.update(rra=output_rgb_meter_dict['rra'],
+                                          rta=output_rgb_meter_dict['rta'])
+                        depth_meter.update(mae=output_rgb_meter_dict['depth_mae'],
+                                           mse=output_rgb_meter_dict['depth_mse'])
+                        pcd_meter.update(mae=output_rgb_meter_dict['pcd_mae'],
+                                         mse=output_rgb_meter_dict['pcd_mse'])
+                        
+                    
+                    pose_results_dict = pose_meter.get_stats()
+                    depth_results_dict = depth_meter.get_stats()
+                    pcd_results_dict = pcd_meter.get_stats()
+
+                    results_dict = {
+                    "Pose_Estimation": pose_results_dict,
+                    "Depth_Estimation": depth_results_dict,
+                    "PointMap Estimation": pcd_results_dict,
+                        
+                    }
+                    
+                    saved_into_json(data_dict=results_dict,
+                                    path=os.path.join(overall_val_batch_save_dir,"metric.json"))
+                        
+                my_model.train()
+                    
+
+            time_e = time.time()
+
+            # print loss log regularly
+            if global_iter % print_freq == 0 and accelerator.is_main_process:
+                lr = optimizer.param_groups[0]['lr']
+                losses_str = ""
+                for loss_k, loss_v in logs.items():
+                    losses_str += ("%s: %.3f, " % (loss_k, loss_v))
+                if logger is not None:
+                    logger.info('[TRAIN] Epoch %d Iter %5d/%d: Loss: %.3f, %s grad_norm: %.1f, lr: %.7f, time: %.3f (%.3f)'%(
+                        epoch, i_iter, len(train_dataloader), 
+                        loss.item(), losses_str, grad_norm, lr,
+                        time_e - time_s, data_time_e - data_time_s
+                    ))
+
+            global_iter += 1
+
+            # dump loss log to tensorboard
+            accelerator.log(logs, step=global_iter)
+
+            data_time_s = time.time()
+            time_s = time.time()
+
+        epoch += 1
+
+    # Create the pipeline using the trained modules and save it.
+    accelerator.wait_for_everyone()
+    accelerator.end_training()
+        
+                    
 if __name__ == '__main__':
     # Training settings
     parser = argparse.ArgumentParser(description='')
