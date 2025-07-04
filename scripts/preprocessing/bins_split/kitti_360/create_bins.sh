89bd7913b5766ecbc6ea39ef2fa94bd54a460505
MagicBoomLiu4GPUs <zliu@ok.sc.e.titech.ac.jp>
2025-07-04 16:48:07 +0900

    fix the logic of fixing the bins with the first camera as the cooridnate

--- scripts/preprocessing/bins_split/kitti_360/create_bins.sh
+++ scripts/preprocessing/bins_split/kitti_360/create_bins.sh
@@ -6,7 +6,7 @@ cd preprocessing/bins_split/kitti360
 root_path="/data1/StereoDatasets/KITTI/KITTI360/"
 filelist_folder="/home/zliu/Project2025/FeedStereoGS/filenames/kitti360/avaliable_lists/"
 min_bin_length=8.0
-out_dir="/data1/StereoDatasets/KITTI/KITTI360/feedforward_bins/"
+out_dir="/data1/StereoDatasets/KITTI/KITTI360/feedforward_bins_first_as_input/"
 
 
 python create_bins.py \
