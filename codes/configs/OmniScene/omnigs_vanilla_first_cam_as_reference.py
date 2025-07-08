0d9080f9044a1390337648f34664eb8863983bb6
MagicBoomLiu4GPUs <zliu@ok.sc.e.titech.ac.jp>
2025-07-08 12:09:39 +0900

    add

--- codes/configs/OmniScene/omnigs_vanilla_first_cam_as_reference.py
+++ codes/configs/OmniScene/omnigs_vanilla_first_cam_as_reference.py
@@ -44,7 +44,7 @@ train_filelist="/home/zliu/Project2025/FeedStereoGS/filenames/kitti360/more_sup_
 val_filelist="/home/zliu/Project2025/FeedStereoGS/filenames/kitti360/trainval/val_2013_05_28_drive_0000_sync.txt"
 test_filelist="/home/zliu/Project2025/FeedStereoGS/filenames/kitti360/trainval/val_2013_05_28_drive_0000_sync.txt"
 sequence='2013_05_28_drive_0000_sync'
-data_version="bin_infos_8.0"
+data_version="bin_infos_8.0_FirstCAM"
 supp_view_nums=3
 camera_model='OpenGL' # select from OpenCV and OpenGL
 
--- scripts/train/omnigs/train.sh
+++ scripts/train/omnigs/train.sh
@@ -1,9 +1,10 @@
 TRAIN_KITTI360_OmniScene_Center_LiDAR_As_Ref_Metric3DV2(){
-cd ../..
+cd ../../..
 cd codes
 configs_path="/home/zliu/Project2025/FeedStereoGS/codes/configs/OmniScene/omnigs_vanilla_center_LiDAR_as_reference.py"
 work_dir="/data1/zliu/feedforward_outputs_new/OmniScene/MetricV2/Center_LiDAR_As_Ref/saved_models"
 resume_from="None"
+# resume_from="/data1/zliu/temp_for_0617/OmniScene/Metric3Dv2/checkpoint-45000/"
 
 #configs 
 # - Single GPU YAML: accelerate_config_singleGPU.yaml
@@ -17,7 +18,7 @@ TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 accelerate la
 }
 
 TRAIN_KITTI360_OmniScene_First_Cam_As_Ref_Metric3DV2(){
-cd ../..
+cd ../../..
 cd codes
 configs_path="/home/zliu/Project2025/FeedStereoGS/codes/configs/OmniScene/omnigs_vanilla_first_cam_as_reference.py"
 work_dir="/data1/zliu/feedforward_outputs_new/OmniScene/MetricV2/First_Cam_As_Ref/saved_models"
@@ -36,7 +37,7 @@ TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 accelerate la
 
 
 TRAIN_KITTI360_OmniScene_Center_LiDAR_As_Ref_NMRFStereo(){
-cd ../..
+cd ../../..
 cd codes
 configs_path="/home/zliu/Project2025/FeedStereoGS/codes/configs/OmniScene/omnigs_NMRF_center_LiDAR_as_reference.py"
 work_dir="/data1/zliu/feedforward_outputs_new/OmniScene/Stereo/Center_LiDAR_As_Ref/saved_models"
@@ -54,7 +55,7 @@ TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 accelerate la
 }
 
 TRAIN_KITTI360_OmniScene_First_Cam_As_Ref_NMRFStereo(){
-cd ../..
+cd ../../..
 cd codes
 configs_path="/home/zliu/Project2025/FeedStereoGS/codes/configs/OmniScene/omnigs_NMRF_first_cam_as_reference.py"
 work_dir="/data1/zliu/feedforward_outputs_new/OmniScene/Stereo/First_Cam_As_Ref/saved_models"
@@ -74,8 +75,8 @@ TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 accelerate la
 
 
 
-TRAIN_KITTI360_OmniScene_Center_LiDAR_As_Ref_Metric3DV2
-TRAIN_KITTI360_OmniScene_First_Cam_As_Ref_Metric3DV2
+#TRAIN_KITTI360_OmniScene_Center_LiDAR_As_Ref_Metric3DV2
+# TRAIN_KITTI360_OmniScene_First_Cam_As_Ref_Metric3DV2
 
-TRAIN_KITTI360_OmniScene_Center_LiDAR_As_Ref_NMRFStereo
+# TRAIN_KITTI360_OmniScene_Center_LiDAR_As_Ref_NMRFStereo
 TRAIN_KITTI360_OmniScene_First_Cam_As_Ref_NMRFStereo
