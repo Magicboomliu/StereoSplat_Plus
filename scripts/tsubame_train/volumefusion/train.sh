b98920c9e37d1f661818fc7cbd08c60a6f07f9e0
MagicBoomLiu4GPUs <zliu@ok.sc.e.titech.ac.jp>
2025-07-10 18:01:36 +0900

    fixed some tsubame bugs

--- scripts/tsubame_train/volumefusion/train.sh
+++ scripts/tsubame_train/volumefusion/train.sh
@@ -41,7 +41,7 @@ TRAIN_KITTI360_VolumeFusion_First_LiDAR_As_Ref_No_Offset(){
 cd ../../..
 cd codes
 configs_path="/home/2/ux04482/FeedStereoGS/codes/configs/Tsubame_Version/Models_Lab/VolumeFusion/VolumeFusion/volumefusion_first_lidar_as_ref_no_offset.py"
-work_dir= "/gs/FeedForwardGS_New/VolumeFusion/First_LiDAR_As_Ref_No_Offset/saved_models"
+work_dir="/gs/FeedForwardGS_New/VolumeFusion/First_LiDAR_As_Ref_No_Offset/saved_models"
 # resume_from="/data1/zliu/temp_for_0617/VolumeFusion/Current_Version/checkpoint-90000/"
 resume_from="None"
 
