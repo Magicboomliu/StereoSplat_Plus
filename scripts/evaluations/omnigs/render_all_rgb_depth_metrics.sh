850d8b236ce6d130cae7489d741413c94d92e9a7
MagicBoomLiu4GPUs <zliu@ok.sc.e.titech.ac.jp>
2025-07-14 18:13:19 +0900

    add

--- scripts/evaluations/omnigs/render_all_rgb_depth_metrics.sh
+++ scripts/evaluations/omnigs/render_all_rgb_depth_metrics.sh
@@ -2,13 +2,13 @@ Get_Rendered_RGBS_Depths_Metrics(){
 cd ../../..
 cd /home/zliu/Project2025/FeedStereoGS/codes/Validation
 
-config_path="/home/zliu/Project2025/FeedStereoGS/codes/configs/OmniScene/eval/omnigs_vanilla_center_LiDAR_as_reference.py"
-output_folder="/home/zliu/Project2025/EvaluationResults/20250714/OmniGS/Center_As_Ref/MetricV2/"
+config_path="/home/zliu/Project2025/FeedStereoGS/codes/configs/OmniScene/eval/omnigs_NMRF_first_LiDAR_as_reference.py"
+output_folder="/home/zliu/Project2025/EvaluationResults/20250714/OmniGS/First_LiDAR_As_Ref/NMRFStereo/"
 val_filelist="/home/zliu/Project2025/FeedStereoGS/filenames/kitti360/trainval/val_2013_05_28_drive_0000_sync_complete.txt"
 demo_filelist="/home/zliu/Project2025/FeedStereoGS/filenames/kitti360/trainval/demo.txt"
-ablation_type="MetricV2" # "MetricV2" or "NMRFStereo"
-dataset_type="Center_LiDAR"
-pretrained_model_path="/data1/zliu/temp_for_0617/OmniScene/Metric3Dv2/checkpoint-45000/"
+ablation_type="NMRFStereo" # "MetricV2" or "NMRFStereo"
+dataset_type="First_LiDAR"
+pretrained_model_path="/data1/zliu/feedforward_outputs_new/OmniScene/Stereo/First_LiDAR_As_Ref/"
 #pretrained_model_path="/data1/zliu/temp_for_0617/OmniScene/NMRFStereo/checkpoint-45000/"
 
 export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:64
