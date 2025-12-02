ee5da261c15b233aca321b42071c4ddd9a7aba4b
MagicBoomLiu4GPUs <zliu@ok.sc.e.titech.ac.jp>
2025-12-02 22:28:46 +0900

    add

--- codes/Difix3D/playground/no_ref_demo.py
+++ codes/Difix3D/playground/no_ref_demo.py
@@ -1,17 +1,22 @@
 import sys
 sys.path.append("..")
-
-
-from src.pipeline_difix import DifixPipeline
+import os
 from diffusers.utils import load_image
+
+from src.simplified_pipeline_diffix_no_ref import DifixPipeline
+
 
 
 if __name__ == "__main__":
     pipe = DifixPipeline.from_pretrained("nvidia/difix", trust_remote_code=True)
     pipe.to("cuda")
-
-    input_image = load_image("/home/zliu/Project2025/OneStageTraining/FeedStereoGS/codes/Difix3D/assets/example_input.png")
+    
+    
+    input_image_path = "/data4/zliu/Difix3D/KITTI360_Degradtations_Pairs/image/0/scene2013_05_28_drive_0000_sync_bin002_19.png"
+    assert os.path.exists(input_image_path), "Input image path does not exist"
+    
+    input_image = load_image(input_image_path)
     prompt = "remove degradation"
 
     output_image = pipe(prompt, image=input_image, num_inference_steps=1, timesteps=[199], guidance_scale=0.0).images[0]
-    output_image.save("/home/zliu/Project2025/OneStageTraining/FeedStereoGS/codes/Difix3D/assets/example_output.png")
+    output_image.save("/home/zliu/Project2025/OneStageTraining/FeedStereoGS/codes/Difix3D/outputs/demo_output/from_pipeline_no_ref.png")
