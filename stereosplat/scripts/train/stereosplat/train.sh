6f570e4e9ad23f06ea89e7d710f417764747677d
MagicBoomLiu4GPUs <zliu@ok.sc.e.titech.ac.jp>
2026-04-13 23:16:48 +0900

    add the stereosplat infra

---
+++ stereosplat/scripts/train/stereosplat/train.sh
@@ -0,0 +1,23 @@
+Train_Stereosplat_On_KITTI360(){
+cd ../../..
+
+pwd
+
+pixi run python trainer/train_kitti360_stereosplat.py
+# cd codes
+# configs_path="/home/zliu/Project2025/FeedStereoGS/codes/configs/Models_Lab/VolumeFusion/volumefusion_revision_complete_kitti360.py"
+# work_dir="/data1/zliu/feedforward_outputs_revision/VolumeFusion/FirwstCAM_As_Ref/KITTI360_Complete/RandomView_RandomSample/saved_models"
+# resume_from="/data1/zliu/feedforward_outputs_revision/VolumeFusion/FirwstCAM_As_Ref/KITTI360_Complete/RandomView_RandomSample/saved_models/checkpoint-93000/"
+
+# #configs 
+# # - Single GPU YAML: accelerate_config_singleGPU.yaml
+# # - Multi GPUs YAML: accelerate_config.yaml
+# export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:64
+# TORCH_USE_CUDA_DSA=1 CUDA_LAUNCH_BLOCKING=1  accelerate launch --config-file accelerate_config.yaml train_kitti360_stereosplat.py \
+#     --py-config $configs_path \
+#     --work-dir  $work_dir \
+#     --resume-from $resume_from
+}
+
+
+Train_Stereosplat_On_KITTI360
